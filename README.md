a79a2bda361937bab368ffbb00127c1babfbcedc
Timmy Willison <tim.willison@thisismedium.com>
2010-09-27 21:23:54 -0400

    performance improvement and separate placeholder plugin

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-jquery.requiredfields.js + HTML5 placeholders<br/>
+jquery.requiredfields.js<br/>
 timmy willison
 
 *Minimalist form validation for all browsers (IE6+, Firefox, Opera, Safari, Chrome)*<br/>
-*HTML5 placeholders across all browsers*
 
 *Version: 1.0, Last updated: 9/23/2010*
 
@@ -15,7 +14,7 @@ License
 
 Copyright (c) 2010 timmy willison<br/>
 Dual licensed under the MIT and GPL licenses.<br/>
-http://timmywillison.com/license/
+<a href="http://timmywillison.com/license/">http://timmywillison.com/license/</a>
 
 Support and Testing
 
@@ -62,52 +61,3 @@ Add class 'reqd-email' to required e-mail fields:
 All the validation does is add class required to empty text fields, 
 select fields with value "NONE", and email fields with absent or invalid email addresses.
 It is the responsibility of the user to make invalid states look how they should.
-
-
-<h1>PURPOSE OF HTML5 PLACEHOLDER PLUGIN</h1>
-
-The purpose of this plugin is to be able to put your placeholders in the HTML5 placeholder attribute and have it work in IE6.  There are several advantages to this.  First, you get to use HTML5.  Second, you can insert values (perhaps you already know what the value of a field should be) into the value attribute without it getting treated like a placeholder by some function you wrote to fake placeholders.  Third, this plugin has even more functionality than the default HTML5 placeholder attribute.  It gives you a class to add placeholder styles.  By default, Safari and Firefox have different default colors for placeholders.  With this plugin, you simply put what color you want placeholder text to be in your own css without worrying about browser-specific selectors.  Plus, you can add other styles when that class is present (which is when the field is blurred).
-
-<h1>PLACEHOLDER USAGE</h1>
-
-<pre>
-$('input:text').outOfPlace();
-</pre>
-
-Options:
-<pre>
-$('input:text').outOfPlace({
-    
-    // Gives you control over the submit function if needed
-    // The default function removes the placeholder before
-    // submitting the form in case the field is not required client-side
-    // This takes care of interfering with any server validation
-    submit: function () {
-        $(this).find('input, textarea').each(function () {
-            var $input = $(this);
-            if($input.val() == $input.data('placeholder'))
-                $input.val('');
-        });
-        return true;
-    },
-
-    // The placeholder class for setting
-    // placeholder styles in your own css
-    // e.g. input.place { color: #666666; }
-    // This creates a lot more flexibility for you and
-    // keeps the js lightweight
-    placeholderClass: 'place'
-});
-</pre>
-  
-Then, your html for all browsers will look like this:
-<pre>
-&lt;input type=&quot;text&quot; placeholder=&quot;Name&quot;/&gt;
-</pre>
-
-<h2>BUT WHY ARE THESE PLUGINS TOGETHER?</h2>
-
-The validation plugin will check that the placeholder is not
-still the value sent if the field is required.
-
-Regardless, you can use either plugin separately.
--- jquery.requiredfields.js
+++ jquery.requiredfields.js
@@ -1,14 +1,13 @@
 /*!
-*   jquery.requiredfields.js + HTML5 placeholders
+*   jquery.requiredfields.js
 *   timmy willison
 *   Minimalist form validation for all browsers (IE6+, Firefox, Opera, Safari, Chrome)
-*   HTML5 placeholders across all browsers
 *   Remember to write your own css for .invalid inputs
 *   Add class 'reqd' to required fields.
 *   Add class 'reqd-email' to required e-mail fields
 */
 
-// *Version: 1.0, Last updated: 9/23/2010*
+// *Version: 1.0, Last updated: 9/27/2010*
 // 
 // Demo         - Coming soon
 // GitHub       - http://github.com/timmywil/requiredfields
@@ -42,8 +41,8 @@
 // You can use this separately in your own submit functions if needed
 $.fn.checkForm = function () {
     function validForm(form) {
-        var allGood = true;
-        var allTags = form.getElementsByTagName("*");
+        var allGood = true,
+            allTags = form.getElementsByTagName("*");
 
         for ( var i=0; i<allTags.length; i++ )
             if( !validTag(allTags[i]) )
@@ -52,8 +51,9 @@ $.fn.checkForm = function () {
         
         
         function validTag(thisTag) {
-            var outClass   = "";
-            var allClasses = thisTag.className.split(" ");
+            var outClass   = "",
+                allClasses = thisTag.className.split(" "),
+                $thisTag   = $(thisTag);
             
             for( var j=0; j<allClasses.length; j++ ) {
                 outClass += validBasedOnClass(allClasses[j]) + " ";
@@ -62,7 +62,7 @@ $.fn.checkForm = function () {
             thisTag.className = outClass;
             
             if( outClass.indexOf("invalid") > -1 ) {
-                $(thisTag).prev('label').addClass('invalid');
+                $thisTag.prev('label').addClass('invalid');
                 return false;
             }
             return true;
@@ -77,12 +77,12 @@ $.fn.checkForm = function () {
                     case "reqd":
                         var v = $.trim(thisTag.value);
                         // Also check if the field's value is the same as its placeholder
-                        if( v === "" || v === "NONE" || v === $(thisTag).data('placeholder') ) {
+                        if( v === "" || v === "NONE" || v === $thisTag.data('placeholder') ) {
                             classBack = "invalid ";
                             
                             // Account for elect plugin
                             if ( thisTag.nodeName === "SELECT" ) {
-                                $(thisTag).closest('div.elect-container').addClass('invalid');
+                                $thisTag.closest('div.elect-container').addClass('invalid');
                             }
                         }
                         classBack += thisClass;
@@ -113,69 +113,14 @@ $.fn.checkForm = function () {
 // Validate a form before it is sent
 $.fn.validate = function () {
     return this.submit(function (e) {
-        var check = $(this).checkForm();
+        var $form = $(this),
+            check = $form.checkForm();
         
-        // Blur for placeholders
+        // Blur for any placeholders
         if ( !check ) {
-            $(this).find('input:text').blur();
+            $form.find('input:text').blur();
         }
         return check;
-    });
-};
-
-// Use the HTML5 placeholder attribute at all times.
-// Validation will check that the placeholder is not
-// still the value sent if field is required
-$.fn.outOfPlace = function (opts) {
-
-    opts = $.extend({
-
-        // Gives you control over the submit function if needed
-        // The default function removes the placeholder before
-        // submitting the form in case the field is not required client-side
-        submit: function () {
-            $(this).find('input, textarea').each(function () {
-                var $input = $(this);
-                if($input.val() == $input.data('placeholder'))
-                    $input.val('');
-            });
-            return true;
-        },
-
-        // The placeholder class for setting
-        // placeholder styles in your own css
-        // e.g. input.place { color: #666666; }
-        // This creates a lot more flexibility for you and
-        // keeps the js lightweight
-        placeholderClass: 'place'
-    }, opts);
-
-    return this.addClass(opts.placeholderClass).each(function () {
-        var input       = $(this);
-        var defaultText = input.attr('placeholder') || '';
-
-        // Set the placeholder data for future reference
-        input.data('placeholder', defaultText);
-
-        // Attribute no longer needed
-        input.removeAttr('placeholder');
-
-        // Set the value if field is empty
-        if ($.trim(input.val()) === '')
-            input.val(defaultText);
-
-        // Focus and blurs, notice the class added and removed
-        input.focus(function () {
-            if (input.val() == defaultText) {
-                input.val('').removeClass(opts.placeholderClass);
-            }
-        }).blur(function () {
-            if ($.trim(input.val()) === '') {
-                input.val(defaultText).addClass(opts.placeholderClass);
-            }
-        })
-        // Bind the submit function
-        .closest('form').submit(opts.submit);
     });
 };
 
